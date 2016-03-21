3878a40a3c0859ba90d31639fabf4cb1f9dea460
Mike Andres <MikeAadmin@MarkerDesignHome-3492.local>
2016-03-21 12:01:33 -0400

    Revert "Bugs and Character"
    This reverts commit b2650f5eb7b904847286e95dc2975373e3680212.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,8 @@
-//*** Working on moving both the enemy bugs and the character. The character should move by about 100 in each direction in order to keep it in the center of each box.
-//The bugs should move in smaller increments to make the move faster. Then once both move, I need define intersection parameters so that the items don't collide.
-//Then I need to defince edge of canvas parameters so the character doesn't move off the page.
-//Then, I need to set the bugs to come back to the start, so that they continue to move across.
-
-
-
 // Enemies our player must avoid
-var Enemy = function(sprite, x, y) {
+var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = x;
-    this.y = y;
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -22,11 +14,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function(sprite, x, y) {
+Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -34,27 +25,12 @@ Enemy.prototype.render = function(sprite, x, y) {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-//Constructor function for Player
-var Player = function(sprite, x, y) {
-   this.x = x;
-   this.y = y;
-   this.sprite = 'images/char-horn-girl.png';
-};
-
-//creating a prototype chain so that all methods of Enemy exist in Player
-Player.prototype = Object.create(Enemy.prototype);
-
-//creating a handleInput method to accept allowedKeys
-Player.prototype.handleInput = function () {
-};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-
-var allEnemies = [new Enemy(this, 10, 143), new Enemy(this, -10, 63), new Enemy(this, -50, 223)];
-
 // Place the player object in a variable called player
-var player = new Player(this, 200, 410);
+
+
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
